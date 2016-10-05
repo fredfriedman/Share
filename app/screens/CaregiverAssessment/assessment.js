6877810a8b561c22dea8f135a8141b9b0e797581
Daniel Song <daniel100333@yahoo.com>
2016-10-05 19:41:23 -0400

    Last minute ghetto hacks

--- app/screens/CaregiverAssessment/assessment.js
+++ app/screens/CaregiverAssessment/assessment.js
@@ -8,7 +8,9 @@ import { ListView,
 import { Col, Row, Grid } from "react-native-easy-grid";
 import Button from 'react-native-button'
 
+var { backIcon, whiteGradient } = require('../../config/images')
 var Header = require('../../components/header').default
+var Log = require('../DailyLog/log').default
 
 
 
@@ -18,10 +20,23 @@ export default class Assessment extends Component {
         super();
     }
 
+    onBack() {
+        this.props.navigator.pop()
+    }
+
     render() {
         return (
         <View style={{ backgroundColor: '#E9E9E9', flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
             <Header text={"Assessment"}/>
+            
+            <TouchableHighlight
+                onPress={()=>this.onBack()}
+                style={{position: 'absolute', width: 20, height: 20, top: 25, left: 15, backgroundColor: 'transparent'}}
+                underlayColor={'#f8f8f8f'}>
+                <Image source={backIcon}/>
+            </TouchableHighlight>
+
+            <Log/>
 
     	</View>
         );
