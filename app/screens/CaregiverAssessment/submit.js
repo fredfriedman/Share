18faaacc304f87f64a64440943fcd60846d7956c
Daniel Song <daniel100333@yahoo.com>
2016-11-13 18:24:43 -0500

    Added base file for Submit page

---
+++ app/screens/CaregiverAssessment/submit.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { ListView,
+        TouchableHighlight,
+        StyleSheet,
+        Text,
+        Image,
+        Dimensions,
+        View, } from 'react-native';
+import Button from 'react-native-button';
+import _ from 'lodash';
+
+import backIcon from '../../config/images';
+import Header from '../../components/header';
+
+
+export default class Submit extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+}
