a204b8c9607f173ec78ecae84c8ffd3ec4c4d9cd
Daniel Song <daniel100333@yahoo.com>
2016-11-16 04:03:53 -0500

    Basic implementation of Styling and UI for the submission screen.

--- app/screens/CaregiverAssessment/submit.js
+++ app/screens/CaregiverAssessment/submit.js
@@ -4,24 +4,72 @@ import { ListView,
         StyleSheet,
         Text,
         Image,
+        Alert,
         Dimensions,
         View, } from 'react-native';
 import Button from 'apsl-react-native-button';
-import _ from 'lodash';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import EStyleSheet from 'react-native-extended-stylesheet';
 
-import backIcon from '../../config/images';
-import Header from '../../components/header';
-
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
 export default class Submit extends Component {
-
     constructor(props) {
         super(props);
     }
 
     render() {
-
+        return(
+            <View style={Styles.container}>
+                <View style={{height: height * 0.3, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={Styles.submissionTitle}>
+                        Congratulations!
+                    </Text>
+                </View>
+                <View style={{height: height * 0.2, flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={Styles.submissionBody}>
+                        You have completed the assessment! Please make sure your answers
+                        are what you want them to be before submitting.
+                    </Text>
+                </View>
+                <Button
+                    style={Styles.submitButton}
+                    textStyle={{color: 'white'}}>
+                    Submit Answers
+                </Button>
+            </View>
+        );
     }
 
 }
+
+var Styles = EStyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  submissionTitle: {
+    flex: 1,
+    fontSize: 50,
+    fontWeight: 'bold',
+    padding: 10, 
+    textAlign: 'center', 
+    color: '$colors.main'
+  },
+  submissionBody: {
+    flex: 1,
+    padding: 10, 
+    textAlign: 'center', 
+    color: '$colors.main'
+  },
+  submitButton: {
+    backgroundColor: '$colors.answerUnselectedBackground',
+    borderColor: '$colors.answerUnselectedBorder',
+    width: width * 0.5,
+    alignSelf: 'center'
+  }
+});
